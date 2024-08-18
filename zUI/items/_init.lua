926fc88302f4df89f90e6879e5621fafca627f37
Soren Anthoine <soren.anth@gmail.com>
2024-08-18 02:47:53 +0200

    add: hover and description

--- zUI/items/_init.lua
+++ zUI/items/_init.lua
@@ -1,1 +1,9 @@
 ItemsData = {}
+
+RegisterNUICallback('zUI-HoverItem', function(id, cb)
+    local actionData = ItemsData[id]
+    if actionData.action then
+        actionData.action(false, true)
+    end
+    cb('ok')
+end)
