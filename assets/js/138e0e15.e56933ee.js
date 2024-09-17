d5c7c2a793823e6748d0ea5da02639cfb046901d
Soren Anthoine <soren.anth@gmail.com>
2024-09-17 22:16:13 +0200

    Deploy website - based on b18d4ffa8a60ef7301a7b33f61420b5e91fb2162

---
+++ assets/js/138e0e15.e56933ee.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkz_documentation=self.webpackChunkz_documentation||[]).push([[921],{1597:e=>{e.exports=JSON.parse('{"name":"@easyops-cn/docusaurus-search-local","id":"default"}')}}]);
--- zUI/items/separator.lua
+++
@@ -1,7 +0,0 @@
----@param Title string @Le titre du separateur
-function zUI:AddSeparator(Title)
-    local Item = {}
-    Item.Type = "separator"
-    Item.Title = Title
-    table.insert(self.Items, Item)
-end
