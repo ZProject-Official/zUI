333896cf9967e6fd3cc90373f2bde9f9896032a5
Soren-git <soren.anth@gmail.com>
2024-09-17 23:39:53 +0200

    fix: theme.json

--- zUI/init.lua
+++ zUI/init.lua
@@ -1,5 +1,5 @@
 RegisterNUICallback("zUI-GetTheme", function(data, cb)
-    local Theme = json.decode(LoadResourceFile(GetCurrentResourceName(), "theme.json"))
+    local Theme = json.decode(LoadResourceFile(GetCurrentResourceName(), "zUI/theme.json"))
     SendNUIMessage({
         action = "zUI-SetTheme",
         data = {
